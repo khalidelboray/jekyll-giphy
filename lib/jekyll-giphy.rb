71526348357f1e56f50a053eaa20190617097cb6
Khalid Mohamed Elborai <37024839+khalidelboray@users.noreply.github.com>
2021-12-03 18:31:19 +0200

    Update jekyll-giphy.rb

--- lib/jekyll-giphy.rb
+++ lib/jekyll-giphy.rb
@@ -17,10 +17,10 @@ module Jekyll
       uri = URI("http://api.giphy.com/v1/gifs/random?api_key=#{@@public_api}&tag=#{@tag}")
       response = Net::HTTP.get(uri)
       json_response = JSON.parse(response)
-      json_raw = json_response['data']
-      @image_height = json_raw['image_height']
-      @image_width = json_raw['image_width']
-      @image = json_raw['image_original_url']
+      json_raw = json_response['data']['images']['original']
+      @image_height = json_raw['height']
+      @image_width = json_raw['width']
+      @image = json_raw['url']
 
       "<img src=\"#{@image}\" height=\"#{@image_height}\" width=\"#{@image_width}\" alt=\"#{@tag}\">"
 
